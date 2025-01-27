a320fba774d2c8dfc8dd16c1623ec87ecea0d01f
Fabro324 <jorge.fabro@iesdoctorbalmis.com>
2025-01-27 14:58:20 +0100

    Cambios en listado animales

--- frontend/Animalia/src/app/animales/animales.page.ts
+++ frontend/Animalia/src/app/animales/animales.page.ts
@@ -13,4 +13,41 @@ export class AnimalesPage implements OnInit {
   ngOnInit() {
   }
 
+  public animales = [
+    'Erizo',
+    'Conejo',
+    'Perro',
+    'Gato',
+    'Pez',
+    'Pájaro',
+    'Tortuga',
+    'Cerdo',
+    'Vaca',
+    'Cabra',
+    'Oveja',
+    'Caballo',
+    'Burro',
+    'Mono',
+    'Elefante',
+    'Jirafa',
+    'León',
+    'Tigre'
+  ];
+  public results = [...this.animales];
+  public showList = false;
+
+  handleInput(event: Event) {
+    const target = event.target as HTMLIonSearchbarElement;
+    const query = target.value?.toLowerCase() || '';
+    this.results = this.animales.filter((d) => d.toLowerCase().includes(query));
+  }
+
+  handleItemClick(result: string) {
+    console.log('Item clicked:', result);
+  }
+
+  handleSearchbarClick() {
+    this.showList = true;
+  }
+
 }
