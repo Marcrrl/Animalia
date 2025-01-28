adcee204bc10c415cc4cbf6c35556874560e6f1e
David Cascales <david.cascales@iesdoctorbalmis.com>
2025-01-28 11:18:06 +0100

    Actualizar la URL de la API en el servicio de animales

--- frontend/Animalia/src/app/services/animales.service.ts
+++ frontend/Animalia/src/app/services/animales.service.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AnimalesService {
+
   private apiUrl = 'http://localhost:9000/api';
 
   constructor(private http: HttpClient) { }
