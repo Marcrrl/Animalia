41d1cfe0d803c0e16d74c74a540267783caacab9
Marc <marriclop5@alu.edu.gva.es>
2025-01-30 12:07:23 +0100

    Funciona

--- backend/src/main/java/com/animalia/spring/controladores/UsuariosControlador.java
+++ backend/src/main/java/com/animalia/spring/controladores/UsuariosControlador.java
@@ -51,7 +51,7 @@ public class UsuariosControlador {
             }
 
             return ResponseEntity.ok()
-                    .contentType(MediaType.IMAGE_JPEG) // Ajusta el tipo según la imagen
+                    .contentType(MediaType.IMAGE_JPEG)
                     .body(resource);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
