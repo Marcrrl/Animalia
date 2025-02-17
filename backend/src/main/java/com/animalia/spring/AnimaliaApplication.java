89580c6a0c6c1959c91214c426bd8226ade63d57
Marc <marriclop5@alu.edu.gva.es>
2025-02-17 13:44:31 +0100

    Ostias he tocado main 2.0

--- backend/src/main/java/com/animalia/spring/AnimaliaApplication.java
+++ backend/src/main/java/com/animalia/spring/AnimaliaApplication.java
@@ -207,13 +207,8 @@ public class AnimaliaApplication {
                                 Rescates r1 = new Rescates(null, e5, u3, a2, "Calle Falsa 123",
                                                 Rescates.Estado.CANCELADO, Rescates.EstadoAnimal.DESCONOCIDO,
                                                 LocalDate.now(), null);
-<<<<<<< Updated upstream
-                                Rescates r2 = new Rescates(null, e1, u1, a1, "Calle Falsa 123",
-                                                Rescates.Estado.EN_PROCESO, Rescates.EstadoAnimal.ENFERMO,
-=======
                                 Rescates r2 = new Rescates(null, e1, u1, a1, "Calle Verdadera 456",
                                                 Rescates.Estado.EN_PROCESO, Rescates.EstadoAnimal.LEVE,
->>>>>>> Stashed changes
                                                 LocalDate.now(), null);
                                 Rescates r3 = new Rescates(null, e2, u2, a3, "Calle Falsa 123",
                                                 Rescates.Estado.FINALIZADO, Rescates.EstadoAnimal.SANO,
@@ -221,13 +216,8 @@ public class AnimaliaApplication {
                                 Rescates r4 = new Rescates(null, e3, u4, a4, "Calle Falsa 123",
                                                 Rescates.Estado.RECIBIDO, Rescates.EstadoAnimal.GRAVE,
                                                 LocalDate.now(), null);
-<<<<<<< Updated upstream
-                                Rescates r5 = new Rescates(null, e4, u5, a5,"Calle Falsa 123",
-                                                Rescates.Estado.NO_RECIBIDO, Rescates.EstadoAnimal.CAPTURADO,
-=======
                                 Rescates r5 = new Rescates(null, e4, u5, a5, "Avenida Sol 654",
                                                 Rescates.Estado.NO_RECIBIDO, Rescates.EstadoAnimal.SANO,
->>>>>>> Stashed changes
                                                 LocalDate.now(), null);
                                 Rescates r6 = new Rescates(null, e6, u6, a1, "Calle Falsa 123",
                                                 Rescates.Estado.NO_APLICA, Rescates.EstadoAnimal.DESCONOCIDO,
