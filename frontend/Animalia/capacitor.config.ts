ab7d029957068fdbc8b9cc949e7dca2d46de7a33
Fabro324 <jorge.fabro@iesdoctorbalmis.com>
2025-02-18 22:05:14 +0100

    Cambios en el splash screen

--- frontend/Animalia/capacitor.config.ts
+++ frontend/Animalia/capacitor.config.ts
@@ -3,7 +3,18 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Animalia',
-  webDir: 'www'
+  webDir: 'www',
+  
+  plugins:{
+    SplashScreen:{
+      launchShowDuration: 2000,
+      backgroundColor:"#ffffff",
+      splashFullScreen: true,
+      splashImmersive: true,
+      showSpinner: false,
+      androidSpinnerStyle: "small",
+    },
+  },
 };
 
 export default config;
