ed67db4a81d93772f0e31fe1a7a5efdf228a799d
Ruben <75427020+Ruben-Serrano2112@users.noreply.github.com>
2025-02-18 14:42:43 +0100

    no se que he hecho

--- backend/src/main/java/com/animalia/spring/controladores/RescatesController.java
+++ backend/src/main/java/com/animalia/spring/controladores/RescatesController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.animalia.spring.Excepciones.RescateNoEcontrada;
-import com.animalia.spring.entidades.Fotos;
 import com.animalia.spring.entidades.Rescates;
 import com.animalia.spring.entidades.DTO.RescateDTO;
 import com.animalia.spring.entidades.DTO.RescateDetalleDTO;
@@ -120,28 +119,6 @@ public class RescatesController {
                 rescateDTO);
         if (rescateActualizado != null) {
             return ResponseEntity.ok(rescateActualizado);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    @GetMapping("/animal/{animalId}/foto")
-    @Operation(summary = "Obtener ID de la foto relacionada a un rescate por ID del animal", description = "Devuelve la ID de la foto relacionada a un rescate buscando por la ID del animal")
-    public ResponseEntity<Long> obtenerFotoIdPorAnimalId(@PathVariable long animalId) {
-        Long fotoId = rescatesServicio.obtenerFotoIdPorAnimalId(animalId);
-        if (fotoId != null) {
-            return ResponseEntity.ok(fotoId);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    @GetMapping("/animal/{animalId}")
-    @Operation(summary = "Obtener ID del rescate por ID del animal", description = "Devuelve la ID del rescate buscando por la ID del animal")
-    public ResponseEntity<Long> obtenerRescateIdPorAnimalId(@PathVariable long animalId) {
-        Long rescateId = rescatesServicio.obtenerRescateIdPorAnimalId(animalId);
-        if (rescateId != null) {
-            return ResponseEntity.ok(rescateId);
         } else {
             return ResponseEntity.notFound().build();
         }
