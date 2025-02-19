512c9d0327d4b25f95e13ea3c8fccd3ce433b5b4
Fabro324 <jorge.fabro@iesdoctorbalmis.com>
2025-02-19 01:36:07 +0100

    Mejora de funcionamiento en el splash screen y la aplicación visualmente

--- frontend/Animalia/android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ frontend/Animalia/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,18 @@
 package io.ionic.starter;
 
+import android.os.Bundle;
+import android.view.WindowManager;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  /*@Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    // Ocultar la barra de notificaciones
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+      WindowManager.LayoutParams.FLAG_FULLSCREEN);
+  }*/
+}
