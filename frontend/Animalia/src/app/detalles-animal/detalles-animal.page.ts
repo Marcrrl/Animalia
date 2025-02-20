42e4be9fc7ade18dfa2a6890d025721f4b82075d
David Cascales <david.cascales@iesdoctorbalmis.com>
2025-02-20 12:16:50 +0100

    terminar de acabar lo de las fotos en el mapa la redirección

--- frontend/Animalia/src/app/detalles-animal/detalles-animal.page.ts
+++ frontend/Animalia/src/app/detalles-animal/detalles-animal.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AnimalesService } from '../services/animales.service';
+import { FotosService } from '../services/fotos.service';
+import { RescatesService } from '../services/rescates.service';
 
 @Component({
   selector: 'app-detalles-animal',
@@ -11,28 +13,53 @@ import { AnimalesService } from '../services/animales.service';
 export class DetallesAnimalPage implements OnInit {
   id: number | null = null;
   animal: any;
+  tipo: any;
+  rescateId: any;
+rescate: any;
   public imagen: string | null = null;
   constructor(
     private route: ActivatedRoute,
-    private animalesService: AnimalesService
+    private animalesService: AnimalesService,
+    private fotosService: FotosService,
+private rescateService: RescatesService
   ) {}
 
   ngOnInit() {
     // Capturar el id_animal desde la ruta
     this.id = Number(this.route.snapshot.paramMap.get('id'));
+    this.tipo = this.route.snapshot.paramMap.get('tipo') || '';
+    if (this.tipo == 'animal') {
+      this.animalesService.getById(this.id).subscribe(
+        (data) => {
+          this.animal = {
+            ...data,
+            estado_conservacion: data.estado_conservacion.replace(/_/g, ' '),
+          };
 
-    this.animalesService.getById(this.id).subscribe(
-      (data) => {
-        this.animal = {
-          ...data,
-          estado_conservacion: data.estado_conservacion.replace(/_/g, ' ')
-        };
+          this.imagen = this.animalesService.obtenerImagenUrl(this.animal.foto);
+        },
+        (error) => {
+          console.error('Error fetching animal details:', error);
+        }
+      );
+    } else if (this.tipo == 'foto') {
 
-        this.imagen = this.animalesService.obtenerImagenUrl(this.animal.foto);
-      },
-      (error) => {
-        console.error('Error fetching animal details:', error);
-      }
-    );
+
+this.fotosService.obtenerRescatePorIdFoto(this.id).subscribe(
+        (data) => {
+          this.rescateId = data;
+          this.rescateService.getById(this.rescateId).subscribe(
+            (data) => {
+              this.rescate = data;
+            },
+            (error) => {
+              console.error('Error fetching rescue details:', error);
+            }
+          );
+        },
+        (error) => {
+          console.error('Error fetching rescue id:', error);
+        });
+    }
   }
 }
