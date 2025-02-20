f42aad7ddbc1eb077d42cf151a0d6cf4ac1ae798
David Cascales <david.cascales@iesdoctorbalmis.com>
2025-02-20 10:08:25 +0100

    Eliminar comentarios y botones innecesarios en la página de la cámara, ya va rescatesssssssssss

--- frontend/Animalia/src/app/Camara/Camara.page.ts
+++ frontend/Animalia/src/app/Camara/Camara.page.ts
@@ -1,6 +1,6 @@
 import { RescatesService } from './../services/rescates.service';
 import { UsuarioService } from './../services/usuario.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { Geolocation } from '@capacitor/geolocation';
@@ -9,6 +9,7 @@ import { FotosService } from '../services/fotos.service';
 import { set } from 'lodash';
 import { Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
+import { firstValueFrom, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-Camara',
@@ -41,7 +42,7 @@ export class CamaraPage implements OnInit {
     'DESCONOCIDO',
     'SANO',
   ];
-
+  rescateOk: boolean = false;
   constructor(
     private http: HttpClient,
     private animalesService: AnimalesService,
@@ -50,12 +51,13 @@ export class CamaraPage implements OnInit {
     private rescatesService: RescatesService,
     private router: Router,
     private toastController: ToastController
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.nombreFoto = '';
     this.descripcion = '';
     this.ubicacion = '';
+    this.rescateOk = false;
     this.animalSeleccionadoId = null;
     this.animalesService.getTotalAnimales().subscribe((animales) => {
       this.animales = animales; // Guardar todos los animales en la lista principal
@@ -80,20 +82,10 @@ export class CamaraPage implements OnInit {
       console.log('Animales:', this.animales);
     });
 
-    // if (this.rescateId) {
-    //   this.rescatesService.getById(this.rescateId).subscribe((data: any) => {
-    //     this.rescate = data;
-    //     this.rescateOriginal = { ...this.rescate };
-    //   });
-    // } else {
-    //   console.error('No se encontró el id del rescate');
-    // }
+
 
     this.obtenerUbicacionPoint();
   }
-
-
-
 
   onRescateClick(rescateId: number) {
     this.rescateId = rescateId;
@@ -114,28 +106,8 @@ export class CamaraPage implements OnInit {
       error: (err) => console.error('Error obteniendo el ID del rescate', err),
     });
   }
-  /*
-    onAnimalChange() {
-  
-  
-      if (this.animalSeleccionadoId) {
-        console.log('Animal seleccionado:', this.animalSeleccionadoId);
-  
-        this.rescatesFiltrados = this.rescates.filter(
-          (rescate) =>
-            rescate.animal.id === this.animalSeleccionadoId &&
-            rescate.estado_rescate === 'NO_ASIGNADO'
-        );
-  
-        console.log('Rescates filtrados:', this.rescatesFiltrados);
-  
-        this.fotos = this.rescatesFiltrados.flatMap((rescate) => {
-          rescate.fotos;
-        });
-  
-        console.log('Fotos:', this.fotos);
-      }
-    }*/
+
+
 
   filtrarRescates() {
     this.rescatesFiltrados = this.rescates.filter(
@@ -146,17 +118,14 @@ export class CamaraPage implements OnInit {
 
     console.log('Rescates :', this.rescatesFiltrados);
     /*no estoy usando esta lista , pero no quitarla*/
-    this.fotosFiltradas = this.rescatesFiltrados.flatMap(rescate =>
+    this.fotosFiltradas = this.rescatesFiltrados.flatMap((rescate) =>
       rescate.fotos.map((foto: any) => ({
         ...foto,
-        url: this.fotosService.obtenerImagenUrl(foto.url_foto)  // Guardamos la URL aquí
+        url: this.fotosService.obtenerImagenUrl(foto.url_foto), // Guardamos la URL aquí
       }))
     );
 
     console.log('Fotos filtradas:', this.fotosFiltradas);
-
-
-
   }
 
   resetRescateSeleccionadoId() {
@@ -181,7 +150,8 @@ export class CamaraPage implements OnInit {
         },
         complete: () => {
           console.log('Proceso de editar rescate completado.');
-
+          console.log('Se va aredirigir a la pagina de inicio');
+          this.finalizarRescate();
         },
       });
   }
@@ -195,82 +165,93 @@ export class CamaraPage implements OnInit {
     toast.present();
   }
 
-  subirRescate() {
-    let rescateOk = false;
-
-    if (this.foto && this.usuarioOriginal && this.nombreFoto && this.descripcion && this.ubicacion && this.estadoAnimal) {
-
-
-      /*PUT*/
-      if (this.rescateId) {
-        console.log('Editando Rescate');
-        this.subirImagen();
-        this.editarRescate();
-        rescateOk = true;
-      } else {
-        console.log('Añadiendo Rescate');
-        /*POST*/
-        if (
-          this.animalSeleccionadoId &&
-          this.estadoAnimal &&
-          this.ubicacion &&
-          this.usuarioOriginal &&
-          this.foto
-        ) {
-          const token = sessionStorage.getItem('token'); // Obtener el token almacenado
-          const headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`, // Enviar el token en el header
-          });
-          // Crear un nuevo rescate
-          const nuevoRescate = {
-            animalId: this.animalSeleccionadoId, // Asegúrate de que el animal esté asignado correctamente
-            //estado_rescate: 'EN PROCESO',     // El estado inicial puede ser "EN PROCESO" o lo que prefieras
-            estadoAnimal: this.estadoAnimal, // Asigna el estado adecuado para el animal
-            //fecha_rescate: null,       // Usa la fecha actual
-            //fotos: [],                       // Agrega las fotos si es necesario
-            ubicacion: this.ubicacion, // Ubicación por defecto
-            usuarioId: this.usuarioOriginal.id, // Información de usuario
-            //empresa: null // Información de la empresa
-          };
-
-          this.rescatesService.añadirRescate(nuevoRescate, headers).subscribe({
-            next: (response) => {
-              this.rescateId = (response.body as any).id; // Aquí accedes al ID dentro del objeto completo
-              console.log('Rescate agregado con ID:', this.rescateId);
-
-
-              this.subirImagen();
-
-              rescateOk = true;
-
-            },
-            error: (error) => {
-              console.error('Error al agregar el rescate', error);
-            },
-            complete: () => {
-              console.log('Proceso de añadir rescate completado.');
-
-
-            },
-          });
+  async subirRescate() {
+    try {
+      if (
+        this.foto &&
+        this.usuarioOriginal &&
+        this.nombreFoto &&
+        this.descripcion &&
+        this.ubicacion &&
+        this.estadoAnimal
+      ) {
+        /*PUT*/
+        if (this.rescateId) {
+          console.log('Editando Rescate');
+          await this.subirImagen();
+          await this.editarRescate();
+          this.finalizarRescate();
         } else {
-          this.presentToast('Falta algun dato');
-          console.log('Faltan datos');
+          console.log('Añadiendo Rescate');
+          /*POST*/
+          if (
+            this.animalSeleccionadoId &&
+            this.estadoAnimal &&
+            this.ubicacion &&
+            this.usuarioOriginal &&
+            this.foto
+          ) {
+            const token = sessionStorage.getItem('token'); // Obtener el token almacenado
+            const headers = new HttpHeaders({
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`, // Enviar el token en el header
+            });
+            // Crear un nuevo rescate
+            const nuevoRescate = {
+              animalId: this.animalSeleccionadoId, // Asegúrate de que el animal esté asignado correctamente
+              //estado_rescate: 'EN PROCESO',     // El estado inicial puede ser "EN PROCESO" o lo que prefieras
+              estadoAnimal: this.estadoAnimal, // Asigna el estado adecuado para el animal
+              //fecha_rescate: null,       // Usa la fecha actual
+              //fotos: [],                       // Agrega las fotos si es necesario
+              ubicacion: this.ubicacion, // Ubicación por defecto
+              usuarioId: this.usuarioOriginal.id, // Información de usuario
+              //empresa: null // Información de la empresa
+            };
+
+            this.rescatesService.añadirRescate(nuevoRescate, headers).subscribe({
+              next:async (response) => {
+                this.rescateId = (response.body as any).id; // Aquí accedes al ID dentro del objeto completo
+                console.log('Rescate agregado con ID:', this.rescateId);
+
+               await this.subirImagen();
+               this.finalizarRescate();
+              },
+              error: (error) => {
+                console.error('Error al agregar el rescate', error);
+              },
+              complete: () => {
+                console.log('Proceso de añadir rescate completado.');
+              },
+            });
+
+            // const response = await firstValueFrom(
+            //   this.rescatesService.añadirRescate(nuevoRescate, headers)
+            // ); // Convertimos el Observable en Promise
+            // this.rescateId = (response as any).id;
+            // console.log('Rescate agregado con ID:', this.rescateId);
+            // await this.subirImagen();
+          } else {
+            this.presentToast('Falta algun dato');
+            console.log('Faltan datos');
+          }
         }
+      } else {
+        this.presentToast('Falta algun dato');
+        console.log('Faltan datos');
       }
-    } else {
-      this.presentToast('Falta algun dato');
-      console.log('Faltan datos');
+
+
+    } catch (error) {
+      console.error('Error en el proceso de rescate:', error);
     }
-    if (rescateOk) {
-      setTimeout(() => { window.location.reload();  }, 1000);
+  }
 
-
-
+  finalizarRescate() {
+    if (this.rescateOk) {
+      setTimeout(() => {
+        window.location.reload();
+      }, 500);
     }
-
-
   }
 
   async obtenerUbicacionPoint() {
@@ -325,56 +306,65 @@ export class CamaraPage implements OnInit {
     }
   }
 
-  subirImagen() {
-    console.log(
-      'Rescate:' + this.rescateId,
-      'usuario:' + parseInt(this.usuarioOriginal.id),
-      'Foto:' + this.nombreFoto,
-      this.descripcion,
-      this.ubicacion
-    );
-    if (
-      this.rescateId &&
-      this.usuarioOriginal &&
-      this.nombreFoto &&
-      this.descripcion &&
-      this.ubicacion
-    ) {
-      const token = sessionStorage.getItem('token');
-      console.log('Token:', token);
-      const headers = new HttpHeaders({
-        Authorization: `Bearer ${token}`,
-      });
-      const formData = new FormData();
+  subirImagen(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      console.log(
+        'Rescate:' + this.rescateId,
+        'usuario:' + parseInt(this.usuarioOriginal.id),
+        'Foto:' + this.nombreFoto,
+        this.descripcion,
+        this.ubicacion
+      );
+      if (
+        this.rescateId &&
+        this.usuarioOriginal &&
+        this.nombreFoto &&
+        this.descripcion &&
+        this.ubicacion
+      ) {
+        const token = sessionStorage.getItem('token');
+        console.log('Token:', token);
+        const headers = new HttpHeaders({
+          Authorization: `Bearer ${token}`,
+        });
+        const formData = new FormData();
 
-      if (this.file) {
-        console.log('Subiendo archivo:', this.file);
-        formData.append('file', this.file);
+        if (this.file) {
+          console.log('Subiendo archivo:', this.file);
+          formData.append('file', this.file);
+        }
+
+        this.http
+          .post('http://localhost:9000/api/subir-imagen', formData, {
+            headers: headers,
+            observe: 'response',
+          })
+          .subscribe({
+            next: async (response) => {
+              if (response.status === 200 && response.body) {
+                console.log('Foto guardada con éxito', response);
+
+                try {
+                  await this.enviarAnimalRescate(); // Esperamos a que termine antes de continuar
+                  resolve();
+                } catch (error) {
+                  console.error('Error en enviarAnimalRescate:', error);
+                  reject(error);
+                }
+              } else {
+                console.error('Error al subir la imagen:', response.statusText);
+                reject(response.statusText);
+              }
+            },
+            error: (error) => {
+              console.error('Error al subir la imagen:', error);
+              reject(error);
+            },
+          });
+      } else {
+        console.log('Faltan datos');
       }
-
-      this.http
-        .post('http://localhost:9000/api/subir-imagen', formData, {
-          headers: headers,
-          observe: 'response',
-        })
-        .subscribe(
-          (response) => {
-            if (response.status === 200 && response.body) {
-              console.log('Foto guardada con éxito', response);
-            } else {
-              console.error('Error al subir la imagen:', response.statusText);
-            }
-          },
-          (error) => {
-            console.error('Error al subir la imagen:', error);
-          }
-        );
-      setTimeout(() => {
-        this.enviarAnimalRescate();
-      }, 1000);
-    } else {
-      console.log('Faltan datos');
-    }
+    });
   }
 
   async convertToFile(webPath: string): Promise<File> {
@@ -386,56 +376,44 @@ export class CamaraPage implements OnInit {
     return file;
   }
 
-  enviarAnimalRescate() {
-    console.log(
-      'Rescate:' + this.rescateId,
-      'usuario:' + parseInt(this.usuarioOriginal.id),
-      this.nombreFoto,
-      this.descripcion,
-      this.ubicacion
-    );
-    const token = sessionStorage.getItem('token'); // Obtener el token almacenado
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`, // Enviar el token en el header
-    });
-    this.fotosService
-      .añadirFoto(
-        this.rescateId,
-        this.usuarioOriginal.id,
+  enviarAnimalRescate(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      console.log(
+        'Rescate:' + this.rescateId,
+        'usuario:' + parseInt(this.usuarioOriginal.id),
         this.nombreFoto,
         this.descripcion,
-        this.ubicacion,
-        headers
-      )
-      .subscribe({
-        next: (response) => {
-          console.log('Foto agregada', response);
-        },
-        error: (error) => {
-          console.error('Error al agregar la foto', error);
-        },
-        complete: () => {
-          console.log('Proceso de añadir foto completado.');
-
-        },
+        this.ubicacion
+      );
+      const token = sessionStorage.getItem('token'); // Obtener el token almacenado
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`, // Enviar el token en el header
       });
+      this.fotosService
+        .añadirFoto(
+          this.rescateId,
+          this.usuarioOriginal.id,
+          this.nombreFoto,
+          this.descripcion,
+          this.ubicacion,
+          headers
+        )
+        .subscribe({
+          next: (response) => {
+            console.log('Foto agregada', response);
+            resolve();
+          },
+          error: (error) => {
+            console.error('Error al agregar la foto', error);
+          },
+          complete: () => {
+            console.log('Proceso de añadir foto completado.');
+
+            this.rescateOk = true;
+          },
+        });
+    });
   }
 
-  //No creo que use este metodo en el lo quitaré
-  // submitForm() {
-  //   const fechaActual = new Date();
-  //   const fechaLegible = fechaActual.toLocaleDateString('es-ES');
-  //   console.log(fechaLegible);
-
-  //   if (this.fotoForm.valid && /* this.file &&*/ this.emailUsuario) {
-  //     const formData = new FormData();
-  //     //formData.append('archivo', this.file, 'foto.jpg'); // Archivo
-  //     formData.append('ubicacion', this.fotoForm.value.ubicacion);
-  //     formData.append('descripcion', this.fotoForm.value.descripcion);
-  //     formData.append('fecha_captura', this.fotoForm.value.fecha_captura);
-  //     formData.append('email_usuario', this.emailUsuario);
-  //     this.http.post('https://tuapi.com/fotos', formData).subscribe();
-  //   }
-  // }
 }
